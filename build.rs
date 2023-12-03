a114d3036778e017e34b536dc6d8f1d73da9f0eb
BeGeWei <grey@pku.edu.cn>
2023-12-04 01:40:55 +0800

    add logo to program and windows

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+extern crate winres;
+
 fn main() {
     slint_build::compile("ui/main.slint").unwrap();
+
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("ui/table.ico");
+        res.compile().unwrap();
+      }
 }
